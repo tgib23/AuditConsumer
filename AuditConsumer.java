ded1f3de6f501d3fc74c6ff0a9508d49508c6662
satoshi yamada <bigt2323@gmail.com>
2018-12-10 23:52:09 +0900

    use naming arg

--- AuditConsumer.java
+++ AuditConsumer.java
@@ -28,6 +28,14 @@ import java.util.*;
 import java.io.InputStream;
 import java.util.regex.*;
 
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.ParseException;
+
 public class AuditConsumer {
   // Set the stream and topic to read from.
   private static final String MAPRFS_URI = "maprfs:///";
@@ -35,15 +43,54 @@ public class AuditConsumer {
   private static final Connection connection = DriverManager.getConnection("ojai:mapr:");
   private static DocumentStore store;
   private static Random random;
+  private static String cluster;
+  private static String db_path;
+  private static int debug;
 
 
   public static void main(String[] args) throws IOException,InterruptedException {
 
-    if (args.length != 2){
-      System.out.println("AuditConsumer <cluster name> <output dbpath>");
-      System.out.println("ex) $ java -cp .:`mapr classpath` AuditConsumer my.cluster.com /tmp/audit_record");
-      System.exit(1);
+    Options options = new Options();
+    Option clusterOption = OptionBuilder
+      .hasArg(true)
+      .withArgName("cluster")
+      .isRequired(true)
+      .withDescription("cluster name")
+      .withLongOpt("cluster")
+      .create("c");
+
+    Option debugOption = OptionBuilder
+      .hasArg(true)
+      .withArgName("debug")
+      .isRequired(true)
+      .withDescription("debug")
+      .withLongOpt("debug")
+      .create("d");
+
+    Option dbPathOption = OptionBuilder
+      .hasArg(true)
+      .withArgName("output_db_path")
+      .isRequired(true)
+      .withDescription("output db path")
+      .withLongOpt("output_db_path")
+      .create("o");
+
+    options.addOption(clusterOption);
+    options.addOption(debugOption);
+    options.addOption(dbPathOption);
+
+    CommandLineParser parser = new BasicParser();
+    CommandLine commandLine = null;
+    try {
+        commandLine = parser.parse(options, args);
+    } catch (ParseException e) {
+        System.err.println("AuditConsumer -cluster <cluster name> -output_db_path <output db path> -debug <0/1>");
+        System.exit(1);
     }
+
+    cluster = commandLine.getOptionValue("c");
+    db_path = commandLine.getOptionValue("o");
+    debug = Integer.parseInt(commandLine.getOptionValue("d"));
 
     Runtime.getRuntime().addShutdownHook(new Thread()
     {
@@ -55,9 +102,6 @@ public class AuditConsumer {
         System.out.println("All done.");
       }
     });
-
-    String cluster = args[0];
-    String db_path = args[1];
 
     if (!MapRDB.tableExists(db_path)) {
       MapRDB.createTable(db_path); // Create the table if not already present
@@ -98,7 +142,7 @@ public class AuditConsumer {
           processRecord(fs, rvalue, value);
         }
       } else {
-	Thread.sleep(1000);
+    Thread.sleep(1000);
       }
     }
   }
@@ -166,10 +210,12 @@ public class AuditConsumer {
        }
        if (tokens.contains("volumeId")) {
          result = result + lvolName;
-       }	
+       }
      }
 
-     System.out.println(result.substring(0, result.length() - 1));
+     if (debug == 1) {
+       System.out.println(result.substring(0, result.length() - 1));
+     }
      store.insertOrReplace(document);
      store.flush();
      return result.substring(0, result.length() - 1);
--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # AuditConsumer
 
 Sample program to consume audit stream and store them directly to MapR-DB JSON.
+
+
+# Manual Execution
+
+```bash
+$ java -cp .:`mapr classpath` AuditConsumer -cluster <cluster name> -output_db_path <output db path> -debug <0/1>");
+```
+# Daemonize Consumer
+
+```bash
+$ javac -cp .:`mapr classpath` AuditConsumer.java -Xlint:deprecation
+$ sudo mkdir /opt/mapr/audit_consumer
+$ sudo cp AuditConsumer* consumer.props /opt/mapr/audit_consumer/
+Edit AuditConsumer.service to specify the correct classpath, cluster name and output db path to 'ExecStart'
+$ sudo cp AuditConsumer.service /etc/systemd/system/
+```
